0e9706498c9fef66d541079531e8ce90367a820d
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2025-01-15 20:39:45 -0700

    Changed site name + started navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate, Link } from 'react-router-dom'
 import React, { useEffect, useState } from 'react';
 // import { getMessage } from './api';
 
 import Home from './pages/Home.js'
 import SignIn from './pages/SignIn.js';
-
+import Navbar from './components/Navbar.js';
 function App() {
-  const [message, setMessage] = useState('');
-
+  // const [message, setMessage] = useState('');
+  // const navigate = useNavigate()
   // useEffect(() => {
   //   getMessage().then((data) => setMessage(data));
   // }, []);
@@ -21,11 +21,18 @@ function App() {
         <div id="logo">
           <img id="logo" src={require("./images/fairShareLogo.webp")} alt="FairShare Logo" />
         </div>
+        {/* <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+          </ul>
+        </nav> */}
+        <Navbar />
       </header>
       <main>
         <BrowserRouter>
           <Routes>
             <Route index element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/sign-in" element={<SignIn />} />
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/sign-in">Sign In</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
