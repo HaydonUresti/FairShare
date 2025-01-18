ef3285a1f7e4a057b052819aadfd9d3252f764f5
Haydon Uresti <97619164+HaydonUresti@users.noreply.github.com>
2025-01-18 00:37:13 -0700

    Added initial registration

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,54 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import dotenv from 'dotenv';
-import path from "path";
-import { fileURLToPath } from "url";
+import dotenv from 'dotenv'
+import path from "path"
+import { fileURLToPath } from "url"
+
+import StudentModel from './models/Student.js'
 
 // Load environment variables
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const app = express()
+app.use(cors())
+app.use(express.json())
+const PORT = process.env.PORT || 5000
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-app.use(cors());
-app.use(express.json());
+
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.error('MongoDB connection error:', err)
+  })
 
+app.post('/register', (req, res) => {
+  console.log('Request body:', req.body)
+  StudentModel.create(req.body)
+    .then(student => {
+      console.log('Student created:', student)
+      res.json(student)
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
 // Test Route
 // app.get('/', (req, res) => {
 //   res.send('Hello from the backend!');
 // });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`)
+});
+
+app.use(express.static(path.join(__dirname, "build")))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
 });
--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,10 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL
 
 export default function SignIn() {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+  const handleSubmit = (e) => {
+    console.log(API_URL)
+    console.log({ name, email, password })
+    e.preventDefault()
+    axios.post(`${API_URL}/register`, { name, email, password })
+      .then(result => console.log(`The result is ${result}`))
+      .catch(err => console.log(err))
+  }
+
   return (
     <>
-      <div ><h1>Sign In</h1>
-        <button>Sign Up Now!</button>
+      <div >
+        <h1>Sign In</h1>
+        <div>
+          <h2>Register</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email">
+                <strong>Name</strong>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter Name"
+                autoComplete="on"
+                name="name"
+                onChange={(name) => setName(name.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">
+                <strong>Email</strong>
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Email"
+                autoComplete="on"
+                name="email"
+                onChange={(email) => setEmail(email.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">
+                <strong>Password</strong>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  name="password"
+                  onChange={(password) => setPassword(password.target.value)}
+                />
+              </label>
+            </div>
+            <button type="submit">Register</button>
+            <p>Already Have an Account?</p>
+            <button>Login</button>
+          </form>
+        </div>
+
+
       </div>
     </>
   )
